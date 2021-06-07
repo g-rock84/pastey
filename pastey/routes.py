cc06a743117d97943657d1c3c709c26fae9e380b
g-rock84 <jeraldscott.84@gmail.com>
2021-06-07 19:46:10 +0000

    line 192: changed route from '/content' to '/view' as that page did not exist

--- pastey/routes.py
+++ pastey/routes.py
@@ -189,7 +189,7 @@ def paste_json():
         }, 200
 
 # GET paste content (cli compatability)
-@app.route('/content', methods = ['GET'])
+@app.route('/view', methods = ['GET'])
 @limiter.limit(config.rate_limit, exempt_when=lambda: common.verify_whitelist(common.get_source_ip(request)))
 def get_paste():
     source_ip = common.get_source_ip(request)
@@ -204,7 +204,7 @@ def get_paste():
     paste_file = config.data_directory + "/" + unique_id
     if path.exists(paste_file):
         paste = json.loads(paste_file)
-        return paste["content"], 200
+        return paste['content'], 200
     else:
         abort(400)
 
